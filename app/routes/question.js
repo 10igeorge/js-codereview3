4e7931a77a566576ed0d39a546d8c097158d52a1
10igeorge <10izzygeorge@gmail.com>
2016-04-01 12:39:22 -0700

    give user ability to delete questions

--- app/routes/question.js
+++ app/routes/question.js
@@ -25,5 +25,17 @@ export default Ember.Route.extend({
       });
       this.transitionTo('/question/'+question.id);
     },
+    deleteQuestion(question){
+      console.log(question);
+      if(confirm("Delete this question?")){
+        var answer_deletions = question.get('answers').map(function(answer){
+          return answer.destroyRecord();
+        });
+        Ember.RSVP.all(answer_deletions).then(function(){
+          return question.destroyRecord();
+        });
+        this.transitionTo('index');
+      }
+    }
   }
 });
